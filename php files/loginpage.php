7e46bbc1ee6ad32d936dac36e08743415b42498c
Long Vu <Longvu2000@csu.fullerton.edu>
2022-09-22 15:40:02 -0700

    Added cool css hahahahhaha

--- php files/loginpage.php
+++ php files/loginpage.php
@@ -12,19 +12,36 @@
     </head>
 
     <body>
+      <div class="login-page">
+      <div class="form">
+      <form class="register-form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+      </form>
+      <form class="login-form">
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+      <p class="message">Not registered? <a href="#">Create an account</a></p>
+    </form>
+    </div>
+    </div>
+      <!--<h1>Login</h1>
 
-        <h1>Login</h1>
-
-        <form method="post">
+      <form method="post">
 
 			<input id="text" type="text" name="user_name"><br><br>
 			<input id="text" type="password" name="password"><br><br>
 
 			<input id="button" type="submit" value="Login"><br><br>
 
-			<a href="Signup.php">Click to Signup</a><br><br>
-		</form>
+			<a href="Signup.php">Click to Signup</a><br><br> 
+		</form> -->
 
     </body>
 
 </html>
+
--- php files/signuppage.php
+++ php files/signuppage.php
@@ -1,33 +1,3 @@
-<?php
-session_start();
-
-if(isset($_POST['username'],$_POST['password'])) {
-	{
-		//something was posted
-		$user_name = $_POST['user_name'];
-		$password = $_POST['password'];
-		
-		$db = new mysqli('localhost','root','','testing');
-
-		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
-		{
-
-			//save to database
-			$sql = mysqli_query($db, "insert into login(p_num,username,password) values('','$username','$password')");
-
-			mysqli_query($con, $query);
-
-			header("Location: loginpage.php");
-			die;
-		}else
-		{
-			echo "Please enter some valid information!";
-		}
-	}
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
