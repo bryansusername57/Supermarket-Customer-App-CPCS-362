51d344ed45367eb4dd6041304053262418a85755
bryansusername57 <bryansaccount57@csu.fullerton.edu>
2022-09-29 15:48:40 -0700

    Current Build - Created a login and sign up page, the sign page takes user input and puts the data into the database.

--- php files/loginpage.php
+++ php files/loginpage.php
@@ -14,18 +14,11 @@
     <body>
       <div class="login-page">
       <div class="form">
-      <form class="register-form">
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
-      <button>create</button>
-      <p class="message">Already registered? <a href="#">Sign In</a></p>
-      </form>
       <form class="login-form">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="Username"/>
+      <input type="password" placeholder="Password"/>
       <button>login</button>
-      <p class="message">Not registered? <a href="#">Create an account</a></p>
+      <p class="message">Not registered? <a href="signuppage.php">Create an account</a></p>
     </form>
     </div>
     </div>
@@ -44,4 +37,3 @@
     </body>
 
 </html>
-
--- php files/signuppage.php
+++ php files/signuppage.php
@@ -1,27 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <link rel="stylesheet" href="style.css">
-
-       <meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css">
+		
+		<meta charset="UTF-8">
 
          <title>Supermarket Customer</title>
 
     </head>
 
-    <body>
+	<body>
+      <div class="login-page">
+      <div class="form">
+      <form action="insert.php" method="post" class="login-form">
+      <input type="text" placeholder="Username" name="username"/>
+      <input type="password" placeholder="Password" name="password"/>
+      <button>Sign Up</button>
+      <p class="message">Already registered? <a href="signuppage.php">Go to Login</a></p>
+    </form>
+    </div>
+    </div>
+
+    <!--<body>
 
         <h1>Sign Up</h1>
 
-        <form method="post">
+        <form action="insert.php" method="post">
 
-			<input id="text" type="text" name="user_name"><br><br>
+			<input id="text" type="text" name="username"><br><br>
 			<input id="text" type="password" name="password"><br><br>
 
-			<input id="button" type="submit" value="Signup"><br><br>
+			<tr>
+			<td><input type="submit" name"submit" value="Sign Up"</td><br><br>
+			</tr>
 
-			<a href="login.php">Click to Login</a><br><br>
-		</form>
+			<a href="loginpage.php">Click to Login</a><br><br>
+		</form>-->
 
     </body>
 
