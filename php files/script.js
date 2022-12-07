5ef3dfd92ba7864d23014bf6a205a190d0e70943
Long Vu <Longvu2000@csu.fullerton.edu>
2022-12-06 18:02:05 -0800

    Fixed Sync
    New nav bar, More buttons, can take away money

--- php files/script.js
+++ php files/script.js
@@ -1,3 +1,29 @@
-$('.message a').click(function(){
-    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
- });
+let searchFrom = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchFrom.classList.toggle('active')
+    shoppingCart.classList.remove ('active')
+    navbar.classList.remove('active')
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => {
+    shoppingCart.classList.toggle('active')
+    searchFrom.classList.remove('active')
+    navbar.classList.remove('active')
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active')
+    searchFrom.classList.remove('active')
+    shoppingCart.classList.remove ('active')
+}
+
+window.onscroll = () => {
+    searchFrom.classList.remove('active')
+    shoppingCart.classList.remove ('active')
+    navbar.classList.remove('active')
+}
